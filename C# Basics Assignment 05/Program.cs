157e1b2706b283f7f2ab1761654a33bb4f3249e5
Mahmoud Mansour <mansour@gmail.com>
2025-07-15 21:37:43 +0300

    Complete First Video - Part 01 For & Foreach

--- C# Basics Assignment 05/Program.cs
+++ C# Basics Assignment 05/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Looping | Iteration Statements
+
+            #region For - Foreach
+
+            //int[] Numbers = { 1, 2, 3, 4, 5 };
+
+            //Console.WriteLine(Numbers[1]);
+            //Console.WriteLine(Numbers[1]);
+            //Console.WriteLine(Numbers[2]);
+            //Console.WriteLine(Numbers[3]);
+            //Console.WriteLine(Numbers[4]);
+            //Console.WriteLine($"Number is {Numbers[0]}");
+            //Console.WriteLine($"Number is {Numbers[1]}");
+            //Console.WriteLine($"Number is {Numbers[2]}");
+            //Console.WriteLine($"Number is {Numbers[3]}");
+            //Console.WriteLine($"Number is {Numbers[4]}");
+            //Console.WriteLine($"Number 0 is {Numbers[0]}");
+            //Console.WriteLine($"Number 1 is {Numbers[1]}");
+            //Console.WriteLine($"Number 2 is {Numbers[2]}");
+            //Console.WriteLine($"Number 3 is {Numbers[3]}");
+            //Console.WriteLine($"Number 4 is {Numbers[4]}");
+
+            //for (int i = 0; i < Numbers.Length; i++)
+            //{
+            //    Console.WriteLine(Numbers[i]);
+            //}
+
+            //for (int i = 0; i < Numbers.Length; i++)
+            //{
+            //    //Numbers[i] += 10;
+            //    if (i == 2)
+            //        break;
+            //        //continue;
+            //    Console.WriteLine($"Number {i} is {Numbers[i]}");
+            //}
+
+            //foreach (int item in Numbers)
+            //{
+            //    //item += 10;
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            #endregion
         }
     }
 }
